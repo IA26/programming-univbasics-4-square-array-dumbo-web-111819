184512c503be37fca403ab7220f5c846923f006a
Isaac Avilez <isaac1180@aol.com>
2019-11-07 13:03:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,15 @@
-def square_array(array)
-  myArray = []	
-    array.each do |n|	
-    myValue = n**2 
-    myArray << myValue	
-   end
-  myArray	
+
+def square_array(numbers)
+  result = Array.new(numbers.length)
+  counter = 0
+
+  while counter < numbers.length
+    result[counter] = numbers[counter] ** 2
+    counter += 1
+  end
+
+  # Return the result array
+  result
 end
+
+square_array([1,2,3,4])
